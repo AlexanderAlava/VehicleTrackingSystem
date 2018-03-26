e3db27402cfdc5b813f855411cac3d708a8923df
Alexander Alava <alava@mail.usf.edu>
2018-03-25 22:56:57 -0400

    Created Cars class and added initial structure

--- VehicleTrackingSystem.cpp
+++ VehicleTrackingSystem.cpp
@@ -1,5 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Cars
+{
+  public:
+
+    // Data members //
+    string brand;
+    string year;
+    string color;
+    string type;
+    string name;
+
+    // Member functions //
+
+};
+
 int main()
 {
-  cout << "This is a car dealership!";
+  cout << "\nThis is a car dealership!\n";
   return 0;
 }

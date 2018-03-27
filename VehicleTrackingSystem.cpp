a2aecf7d8aed9ab9a6a6c86382c235cc77671c37
Alexander Alava <alava@mail.usf.edu>
2018-03-27 16:04:27 -0400

    Added basic function declaration and initial menu options

--- VehicleTrackingSystem.cpp
+++ VehicleTrackingSystem.cpp
@@ -1,23 +1,74 @@
 #include <iostream>
 using namespace std;
 
-class Cars
+class Car
 {
   public:
 
     // Data members //
-    string brand;
-    string year;
-    string color;
     string type;
-    string name;
+    string make;
+    string model;
+    string year;
+    int count;
 
     // Member functions //
+    Car(string type, string make, string model, string year);
+    void addCar();
+    void deleteCar();
+    void sellCar();
+    void searchCar();
+    void printInventory();
+    ~Car();
 
 };
 
+Car::Car(string type, string make, string model, string year)
+{
+  type = type;
+  make = make;
+  model = model;
+  year = year;
+}
+
+void Car::addCar()
+{
+  string tempType;
+  string tempMake;
+  string tempModel;
+  string tempYear;
+
+  cout << "Please enter the following information regarding the vehicle:\n";
+  cout << "Type: ";
+  cin >> tempType;
+  cout << "Make: ";
+  cin >> tempMake;
+  cout << "Model: ";
+  cin >> tempModel;
+  cout << "Year: ";
+  cin >> tempYear;
+
+  Car car1(tempType, tempMake, tempModel, tempYear);
+}
+
+Car::~Car()
+{
+
+}
+
 int main()
 {
-  cout << "\nThis is a car dealership!\n";
+  int option = 0;
+
+  cout << "Welcome to this car dealership!\n\n";
+
+  cout << "(1) Add a car batch to the inventory.\n";
+  cout << "(2) Delete a car batch from the inventory.\n";
+  cout << "(3) Complete a sale.\n";
+  cout << "(4) Search for a specific car in the inventory.\n";
+  cout << "(5) Print current availability of all vehicles in the inventory.\n\n";
+  cout << "Please enter your selection or enter anything else to exit the program: ";
+  cin >> option;
+
   return 0;
 }
